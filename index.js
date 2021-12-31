0bfb75568870f8dfcc82f9757f5a0d53e8d058f6
vinay7722 <vchauhan765@rku.ac.in>
2021-12-31 08:33:43 +0530

    que-3

---
+++ index.js
@@ -0,0 +1,18 @@
+var sum = (a,b)=>{
+    return a+b;
+}
+
+var mul = () =>{
+    return a*b; 
+}
+
+var sub = () => {
+    return a-b;
+}
+
+console.log("this is index.js file")
+module.exports = (sum,mul,sub);
+
+
+
+    
